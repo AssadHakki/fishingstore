915d32cdb76b19dcb2ec8cd019ab6e2162e48cca
AssadHakki <hakki.assad@gmail.com>
2021-04-11 19:09:08 +0000

    the second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-// import logo from './logo.svg';
-// import './App.css';
+
+import React from 'react';
+import Header from './components/Header';
+import Homepage from './pages/Homepage';
+import './default.scss';
+
 
 function App() {
   return (
     <div className="App">
-     <h3>Hello world</h3>
+        <Header/>
+      <div className="main">
+        <Homepage/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Directory/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Shopping from './../../assets/homepage_bg.jpeg';
+import './styles.scss';
+const Directory = props => {
+    return (
+        <div className="directory">
+            <div className="wrap">
+                <div className="item"              
+                    style={{ backgroundImage: `url(${Shopping})`}}  >                        
+                    <a>
+                    Join the store
+                    </a>    
+                </div>  
+            </div>
+        </div>
+
+    );
+};
+
+export default Directory;
