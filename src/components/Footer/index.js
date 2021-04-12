633e008977b96b864ac9e27a0a0a26ca773bf842
AssadHakki <hakki.assad@gmail.com>
2021-04-12 20:44:26 +0000

    4th save in the second day

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.scss';
+
+const Footer = props => {
+    return (
+        <footer className="footer">
+            <div className="wrap">
+                 Made with love by ASSAD HAKKI © Fish'ON  2021
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
